aa77146fee5616921e432e9e65a545fc57b58a00
Zhenya Kulinich <kulinichevgeny@gmail.com>
2024-05-18 20:44:46 +0300

    Added prettier configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,11 +16,11 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-    <body className={ font.className }>{ children }</body>
+      <body className={font.className}>{children}</body>
     </html>
   )
 }
