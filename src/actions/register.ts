e26b91152737a4005fa73f782d43af4a905daa63
Zhenya Kulinich <kulinichevgeny@gmail.com>
2024-05-19 20:31:01 +0300

    Added password hash encryption

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,8 +1,11 @@
 'use server'
 
 import * as z from 'zod'
+import bcrypt from 'bcrypt'
 
 import { RegisterSchema } from '@/schemas'
+import { prisma } from '@/lib/db'
+import { getUserByEmail } from '@/helpers/users'
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values)
@@ -10,6 +13,21 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validatedFields.success) {
     return { error: 'Something went wrong! Try again' }
   }
+
+  const { name, email, password } = validatedFields.data
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const existingUser = await getUserByEmail(email)
+
+  if (existingUser) return { error: 'Email already in use!' }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword
+    }
+  })
 
   return { success: 'Successfully registered!' }
 }
