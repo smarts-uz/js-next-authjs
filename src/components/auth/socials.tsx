1c0bd96dfc06c34e52d826c59687e4791632bb58
Zhenya Kulinich <kulinichevgeny@gmail.com>
2024-05-20 13:56:39 +0300

    Implemented github & google oauth

--- src/components/auth/socials.tsx
+++ src/components/auth/socials.tsx
@@ -1,17 +1,33 @@
 'use client'
 
+import { signIn } from 'next-auth/react'
 import { FcGoogle } from 'react-icons/fc'
 import { FaGithub } from 'react-icons/fa'
 
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes'
 import { Button } from '@/components/ui/button'
 
 export const Socials = () => {
+  const handleAuth = (provider: 'google' | 'github') => {
+    signIn(provider, { callbackUrl: DEFAULT_LOGIN_REDIRECT })
+  }
+
   return (
     <div className="flex items-center w-full gap-x-2">
-      <Button className="w-full" variant="outline" size="lg" onClick={() => {}}>
+      <Button
+        className="w-full"
+        variant="outline"
+        size="lg"
+        onClick={() => handleAuth('google')}
+      >
         <FcGoogle className="h-5 w-5" />
       </Button>
-      <Button className="w-full" variant="outline" size="lg" onClick={() => {}}>
+      <Button
+        className="w-full"
+        variant="outline"
+        size="lg"
+        onClick={() => handleAuth('github')}
+      >
         <FaGithub className="h-5 w-5" />
       </Button>
     </div>
